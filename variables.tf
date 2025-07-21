88ccfd0b658a211ba535624e94c258f21f5bbd69
Derradji Senani <89805807+senani-derradji@users.noreply.github.com>
2025-07-21 17:05:19 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "subid" {
   type        = string
-  default     = "d6d5f527-564e-46e8-a8c1-1853446b9d96"
+  default     = "d6d5f527-564e-46e8-a8c1-1853446b9d96" # old SandBox Subscription xD
   description = "Subscription ID for Provider Resource"
 }
 
 variable "SBResource" {
   type        = string
-  default     = "learn-772a3ecb-5dd5-46ff-b5ec-689210c6f9a2"
+  default     = "learn-772a3ecb-5dd5-46ff-b5ec-689210c6f9a2" # old SandBox Resource Group xD
   description = "SandBox Resource Group Name"
 }
